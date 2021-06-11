a65fbbf0318cf65f7e3a08afece004ce0c26958c
Tên ng0cminh <bnng0cminh@yahoo.com>
2021-06-11 15:35:15 +0700

    Trick Middleware

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const handlebars = require('express-handlebars');
 // Import Connect MongoDB
 const db = require('./config/db');
 
-const sortMiddleware = require('./app/middlewares/SortMiddleware');
+const sortMiddleware = require('./app/middlewares/sortMiddleware.x');
 
 // Import Router
 const router = require('./routes');
