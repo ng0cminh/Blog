8a4e4ce860cd31978d1e424f1c3fe66342604e42
ng0cminh <ng0cminh@yahoo.com>
2021-06-17 15:33:54 +0700

    login and register

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -12,41 +12,48 @@ class UserController {
    // [POST] /register
    async createUser(req, res, next) {
       const formData = req.body;
-      const { username, password } = formData;
+      const { username, password, email } = formData;
 
       // Simple validation
-      if (!username || !password) {
+      if (!username || !password || !email) {
          return res.status(400).json({
             success: false,
-            message: 'Missing username and/or password',
+            message: 'Missing username and/or password and/or email',
          });
       } else {
          try {
             // Check for existing user
             const user = await User.findOne({ username });
-
             if (user) {
                return res
                   .status(400)
                   .json({ success: false, message: 'Username already taken' });
-            } else {
-               // All good
-               const hashedPassword = await argon2.hash(password);
-               formData.password = hashedPassword;
-               const newUser = new User(formData);
-               await newUser.save();
-
-               // Return token
-               const accessToken = jwt.sign(
-                  { userId: newUser._id },
-                  process.env.ACCESS_TOKEN_SECRET,
-               );
-
-               res.json({
-                  message: 'luu duoc',
-                  accessToken: accessToken,
-               });
             }
+
+            // Check for existing email
+            const emailvalid = await User.findOne({ email });
+            if (emailvalid) {
+               return res
+                  .status(400)
+                  .json({ success: false, message: 'Email already taken' });
+            }
+
+            // All good
+            const hashedPassword = await argon2.hash(password);
+            formData.password = hashedPassword;
+            const newUser = new User(formData);
+            await newUser.save();
+
+            // Return token
+            const accessToken = jwt.sign(
+               { userId: newUser._id },
+               process.env.ACCESS_TOKEN_SECRET,
+            );
+
+            res.json({
+               message: 'luu duoc',
+               accessToken: accessToken,
+            });
          } catch (error) {
             console.log(error);
             res.status(500).json({
